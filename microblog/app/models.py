bd241152eb4dae8b4ae245993f4ea057bfff06e4
asereme <abdel.sereme@optum.com>
2017-06-29 10:52:55 -0500

    Adding db migration files and code

---
+++ microblog/app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index=True, unique = True)
+
+
+# Way to print object content
+    def __rep__(self):
+        return("<User %r>" % (self.nickname))
